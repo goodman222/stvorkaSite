29684f2aba9af0cc3953d353287265fb613c6add
goodman222 <107044479+goodman222@users.noreply.github.com>
2023-03-24 00:54:52 +0700

    catalog + menu

---
+++ project/catalog/script.js
@@ -0,0 +1,26 @@
+
+document.querySelector('.filter-open-button').onclick = function () {
+  openCloseItem('filter-open');
+}
+
+document.querySelector('.close-icon').onclick = function () {
+  openCloseItem('filter-open');
+}
+
+
+
+// Дроп даун открыть закрыть
+
+document.querySelector('.filter').addEventListener('click', function (event) {
+  let target = event.target;
+  console.log(target);
+  let isItIcon = target.classList.contains('dropdown-icon');
+  let isItCheckBox = target.parentNode.parentNode.classList.contains('check-box');
+  if (isItIcon) {
+    target.parentNode.parentNode.classList.toggle('dropdown-closed');
+  } else if (isItCheckBox) {
+    target.parentNode.parentNode.classList.toggle('check-box-active');
+    }
+});
+
+// NOTE: Не србатывает на верхних чекбоксах.
